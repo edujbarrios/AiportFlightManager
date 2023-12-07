d8682a8196d512273b908745ef1097f8530170e5
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-07 15:24:55 +0000

    Adding more Tests as required at pr6

--- src/test/java/es/ull/flights/VuelosTest.java
+++ src/test/java/es/ull/flights/VuelosTest.java
@@ -106,4 +106,84 @@ public class VuelosTest {
         vueloConUnAsiento.addPassenger(pasajero1);
         assertThrows(RuntimeException.class, () -> vueloConUnAsiento.addPassenger(pasajero2));
     }
+
+    @Test
+    @DisplayName("Crear vuelo con número de vuelo en minúsculas lanza excepción")
+    void crearVueloConNumeroEnMinusculas() {
+        assertThrows(RuntimeException.class, () -> new Flight("ib001", 150));
+    }
+    @Test
+    @DisplayName("Intentar eliminar un pasajero que no está en el vuelo no cambia la cantidad de pasajeros")
+    void intentarEliminarPasajeroNoExistente() {
+        int numeroPasajerosInicial = vuelo.getNumberOfPassengers();
+        vuelo.removePassenger(new Passenger("999", "Pedro", "US"));
+        assertEquals(numeroPasajerosInicial, vuelo.getNumberOfPassengers());
+    }
+    @Test
+    @DisplayName("Crear vuelo con número de vuelo nulo lanza excepción")
+    void crearVueloConNumeroNulo() {
+        assertThrows(RuntimeException.class, () -> new Flight(null, 150));
+    }
+    @Test
+    @DisplayName("Crear vuelo con número negativo de asientos lanza excepción")
+    void crearVueloConAsientosNegativos() {
+        assertThrows(RuntimeException.class, () -> new Flight("IB0-4", -1));
+    }
+    @Test
+    @DisplayName("Agregar un pasajero nulo lanza excepción")
+    void agregarPasajeroNulo() {
+        assertThrows(RuntimeException.class, () -> vuelo.addPassenger(null));
+    }
+    @Test
+    @DisplayName("Vuelo alcanza capacidad máxima correctamente")
+    void vueloAlcanzaCapacidadMaxima() {
+        vuelo.addPassenger(pasajero1);
+        vuelo.addPassenger(pasajero2);
+        assertEquals(2, vuelo.getNumberOfPassengers());
+    }
+    @Test
+    @DisplayName("Crear vuelo con número de vuelo de longitud mínima y máxima")
+    void crearVueloConLongitudNumeroVuelo() {
+        Flight vueloMin = new Flight("AB123", 10);
+        Flight vueloMax = new Flight("AB1234", 10);
+        assertEquals("AB123", vueloMin.getFlightNumber());
+        assertEquals("AB1234", vueloMax.getFlightNumber());
+    }
+
+    @Test
+    @DisplayName("Agregar pasajeros con el mismo nombre pero diferente ID")
+    void agregarPasajerosNombreIgualIdDiferente() {
+        Passenger pasajeroDuplicado = new Passenger("789", "Lucía", "ES");
+        vuelo.addPassenger(pasajero1);
+        assertTrue(vuelo.addPassenger(pasajeroDuplicado));
+        assertEquals(2, vuelo.getNumberOfPassengers());
+    }
+    @Test
+    @DisplayName("Crear vuelo con cero asientos")
+    void crearVueloConCeroAsientos() {
+        Flight vueloCeroAsientos = new Flight("IB005", 0);
+        assertThrows(RuntimeException.class, () -> vueloCeroAsientos.addPassenger(pasajero1));
+    }
+
+    @Test
+    @DisplayName("Intentar agregar pasajeros más allá de la capacidad máxima lanza excepción")
+    void agregarPasajerosMasAllaCapacidad() {
+        Flight vueloUnAsiento = new Flight("IB006", 1);
+        vueloUnAsiento.addPassenger(pasajero1);
+        assertThrows(RuntimeException.class, () -> vueloUnAsiento.addPassenger(pasajero2));
+    }
+    @Test
+    @DisplayName("Verificar lista de pasajeros después de múltiples adiciones y eliminaciones")
+    void verificarListaPasajerosDespuesEliminaciones() {
+        vuelo.addPassenger(pasajero1);
+        vuelo.addPassenger(pasajero2);
+        vuelo.removePassenger(pasajero1);
+        vuelo.addPassenger(pasajero1);
+        assertEquals(2, vuelo.getNumberOfPassengers());
+    }
+    @Test
+    @DisplayName("Crear vuelo con número de vuelo con caracteres especiales lanza excepción")
+    void crearVueloConCaracteresEspeciales() {
+        assertThrows(RuntimeException.class, () -> new Flight("IB@007", 10));
+    }
 }
