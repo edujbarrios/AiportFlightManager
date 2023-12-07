dc1b7f6fb5f2401127c31b85013c4f125b649cd3
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-07 15:44:25 +0000

    Update Passenger.java

--- src/main/java/es/ull/passengers/Passenger.java
+++ src/main/java/es/ull/passengers/Passenger.java
@@ -1,9 +1,11 @@
 package es.ull.passengers;
+
 import java.util.Arrays;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 import es.ull.flights.Flight;
+
+
 public class Passenger {
     private String identifier;
     private String name;
